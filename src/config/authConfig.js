c0be3479ff2d9c102d3a696724c011cbd3f8869d
Deepak Patel <deepak.patel@mobileprogramming.com>
2025-01-29 15:09:52 +0530

    working on msal

--- src/config/authConfig.js
+++ src/config/authConfig.js
@@ -1,7 +1,7 @@
 export const msalConfig = {
     auth: {
-        clientId: "54b8cd30-fb65-4e60-8f41-ca7a8dcf4938", // Replace with your Client ID
-        authority: "https://login.microsoftonline.com/7e315b15-f710-4dd7-adcb-2738eee4c5e3", // Replace with your Tenant ID
+        clientId: "<<Client ID>>", // Replace with your Client ID
+        authority: "https://login.microsoftonline.com/<your Tenant ID>", // Replace with your Tenant ID
         redirectUri: "http://localhost:5173/login", // Your redirect URL
     },
 };
